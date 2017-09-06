9058e5da5e764fa93226b787a34d8dc40f48bbde
Gabe Jackson <gj@mail.co.de>
2017-09-06 17:51:51 -0400

    Half-finished migration to in-browser Mocha tests

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,11 @@
+const expect = chai.expect;
+
 describe('Beatles Loops', function() {
   it("returns an array of strings containing what instruments each instrument plays", function() {
     const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-    expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
+    expect(theBeatlesPlay(musicians, instruments)).to.have.members(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
   });
 
   describe('johnLennonFacts', function(){
@@ -13,22 +15,22 @@ describe('Beatles Loops', function() {
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"
-      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+      ])).to.have.members(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
 
       expect(johnLennonFacts([
         "foo",
         "bar",
-      ])).toEqual(["foo!!!", "bar!!!"])
+      ])).to.have.members(["foo!!!", "bar!!!"])
     });
   });
 
   describe('iLoveTheBeatles', function() {
     it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
-      expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+      expect(iLoveTheBeatles(7)).to.have.members(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
     });
 
     it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
-      expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
+      expect(iLoveTheBeatles(17)).to.have.members(["I love the Beatles!"]);
     });
   });
 
